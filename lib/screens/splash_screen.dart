624f216803508e865c3298b49b2fe3e71b9c47fe
Doooaa <d98744330@gmail.com>
2024-02-17 00:30:19 +0200

    change splash put login in f-screens

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:medica/pages/Login.dart';
+import 'package:medica/screens/login/Login.dart';
 import 'package:medica/styles/AppColor.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
@@ -12,7 +12,7 @@ class Splash_screen extends StatelessWidget {
     return Scaffold(
       body: AnimatedSplashScreen(
         duration: 1000,
-        splashIconSize: MediaQuery.sizeOf(context).width*0.86,
+        splashIconSize: MediaQuery.sizeOf(context).width*0.8,
         splash: Image.asset(
           'assets/images/onboarding/logo.png',
           fit: BoxFit.cover,
