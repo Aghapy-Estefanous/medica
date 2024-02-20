660eceb114dbf34b414f03b4144d597485e93c5d
Aghapy-Estefanous <105273993+Aghapy-Estefanous@users.noreply.github.com>
2024-02-20 16:05:52 +0200

    Revert "State mangement branch"

--- lib/shared/network/remote/endpoint.dart
+++
@@ -1,7 +0,0 @@
-const LOGIN ='login';
-const Home ='home';
-const Get_Categories ='categories';
-const FAVORITES ='Favorites';
-const PROFILE ='profile';
-const REGISTER ='register';
-const SEARCH ='products/search';
