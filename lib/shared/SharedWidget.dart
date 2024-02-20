da743110b8cf901755fbbbd592d72e5fae2d3bf3
Doooaa <d98744330@gmail.com>
2024-02-20 16:38:10 +0200

    add bloc

---
+++ lib/shared/SharedWidget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:medica/screens/auth/login/login.dart';
+import 'package:medica/shared/network/local/sharedPref.dart';
+
+
+
+
+//   from .......> to
+void navigateToScreen(context, ScreenName) {
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ScreenName,
+      ));
+}
+
+void navigateandFinish(context, ScreenName) {
+  Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ScreenName,
+      ));
+}
+
+dynamic showtoast({required context, required String Message, required color}) {
+  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: color,
+      content: Text(
+        Message,
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+            fontSize: 17, fontWeight: FontWeight.w500, color: Colors.white),
+      )));
+}
+
+logout(context) {
+  return TextButton(
+    child: Text('logout'),
+    onPressed: () {
+      //    Text(CachHelper.getdata(key: 'token',),style: TextStyle(color: Colors.black45),);
+      CachHelper.removeKey(key: 'token');
+      navigateandFinish(context, Login());
+    },
+  );
+}
---
+++ lib/shared/cubit/State.dart
@@ -0,0 +1,51 @@
+
+abstract class AppState {}
+
+class InitialState extends AppState {}
+
+class ChangeBottomNavigateBarState extends AppState {}
+//class shopInitialState extends shopState {}
+
+// class shopHomeLoadingState extends shopState {}
+
+// class shopHomeSuccessgState extends shopState {
+//   MyHomeModel? model;
+//   shopHomeSuccessgState(this.model);
+// }
+
+// class shopHomeErrorState extends shopState {
+//   final String error;
+//   shopHomeErrorState(this.error);
+// }
+
+
+
+
+//user data states
+// class GetUserLoadingState extends shopState {}
+
+// class GetUserSuccessState extends shopState {
+//   UserModel model;
+//   GetUserSuccessState(this.model);
+// }
+
+// class GetUserErrorState extends shopState {
+//   final String error;
+//   GetUserErrorState(this.error);
+// }
+//user data states
+// class RegisterLoadingState extends shopState {}
+// class RegisterSuccessState extends shopState {
+//    loginModel? model;
+//    RegisterSuccessState(this.model);
+
+  
+// }
+
+// class RegisterErrorState extends shopState {
+//   final String error;
+//   RegisterErrorState(this.error);
+// }
+//    class RsgisterVisiablityIconState extends shopState{}
+
+
