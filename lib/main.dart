84bdf402f11fa9f7d6cf799c55fce907735cf3de
Doooaa <d98744330@gmail.com>
2024-03-23 07:48:22 +0200

    add endpoint api

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:medica/home_layout.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-<<<<<<< HEAD
-=======
-import 'package:medica/screens/auth/ConfirmEmailS/cubit/confirm_email_cubit.dart';
-import 'package:medica/screens/auth/ConfirmEmailS/cubit/email_cubit.dart';
-import 'package:medica/screens/auth/NewPasswordS/cubit/new_password_cubit.dart';
-import 'package:medica/screens/static_pages/testing.dart';
->>>>>>> 04b19bf5e28a20c6baa63816a3541857b69fcab9
 import 'package:medica/shared/cubit/Cubit.dart';
 import 'package:medica/core/api/dioConsumer.dart';
 import 'package:medica/shared/styles/themes.dart';
@@ -16,6 +9,7 @@ import 'package:medica/screens/splash_screen.dart';
 import 'package:medica/screens/see_all/see_all.dart';
 import 'package:medica/screens/home/home_screen.dart';
 import 'package:medica/shared/cubit/blocObservser.dart';
+import 'package:medica/screens/static_pages/testing.dart';
 import 'package:medica/shared/network/local/sharedPref.dart';
 import 'package:medica/screens/reservation/ticketScreen.dart';
 import 'package:medica/shared/network/remote/Dio_helper.dart';
@@ -25,14 +19,17 @@ import 'package:medica/screens/medical_history/medical_history.dart';
 import 'package:medica/screens/auth/login_auth/cubit/loginCubit.dart';
 import 'package:medica/screens/medical_history/medical_testsScreen.dart';
 import 'package:medica/screens/auth/ConfirmEmailS/cubit/email_cubit.dart';
+import 'package:medica/screens/auth/ConfirmEmailS/cubit/email_cubit.dart';
 import 'package:medica/screens/medical_history/all_prescriptionsScreen.dart';
 import 'package:medica/screens/auth/register_auth/cubit/register_cubit.dart';
 import 'package:medica/screens/auth/NewPasswordS/cubit/new_password_cubit.dart';
+import 'package:medica/screens/auth/NewPasswordS/cubit/new_password_cubit.dart';
+import 'package:medica/screens/auth/ConfirmEmailS/cubit/confirm_email_cubit.dart';
 import 'package:medica/screens/auth/ConfirmEmailS/cubit/confirm_email_cubit.dart';
 
 final BaseAPI = 'http://medicalsystem-001-site1.ftempurl.com';
 
-main() async{
+main() async {
   Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   dio_helper.init();
